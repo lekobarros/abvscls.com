b9e91196134894bab67ef009d6aa60d525865e72
Alex Vasconcelos <alex.vasconcelos@convenia.com.br>
2024-04-17 00:10:57 -0300

    feat: hero 2

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -1,24 +1,19 @@
 <script lang="ts" setup>
 import gsap from '@/src/plugins/gsap'
 import useGlobalStore from '@/src/store/global'
-import { ref, onMounted, watch } from 'vue'
+import { onMounted, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 
-interface PosTrackMouse {
-  clientX: number
-  clientY: number
-}
 
 // Components
 import TheHeroText from './TheHeroText.vue'
-import TheHeroFigure from './TheHeroFigure.vue'
-import TheHeroCircle from './TheHeroCircle.vue'
+import TheHeroPicture from './TheHeroPicture.vue'
+import TheHeroSpinner from './TheHeroSpinner.vue'
 import TheHeroContact from './TheHeroContact.vue'
 
 // General State
 const globalStore = useGlobalStore()
 const { isLoaded } = storeToRefs(globalStore)
-const posTrackMouse = ref<PosTrackMouse>({ clientX: 0, clientY: 0 })
 
 watch(isLoaded, newVal => {
 	if (newVal) createHeroAnimation()
@@ -33,35 +28,27 @@ const createHeroAnimation = (): void => {
 	const tl = gsap.timeline({ paused: !0, onComplete: () => globalStore.setLoad(true) })
 	tl.addLabel('start', '>')
 
-	tl.heroFadeIn('.hero-text-heading', {}, 'start')
-	tl.heroFadeIn('.hero-text-subtitle', {}, 'start+=0.1')
-	tl.heroFadeIn('.hero-figure', {}, 'start+=0.05')
+	tl.heroFadeIn('.c-hero__text-heading', {}, 'start')
+	tl.heroFadeIn('.c-hero__text-subtitle', {}, 'start+=0.1')
+	tl.heroFadeIn('.c-hero__picture', {}, 'start+=0.05')
 
 	tl.play()
 }
 
 const doResetAnimation = (): void => {
-	gsap.set('.hero-text-heading', { opacity: 0, translateY: '15%', rotate: '1deg' })
-	gsap.set('.hero-text-subtitle', { opacity: 0, translateY: '15%', rotate: '1deg' })
-	gsap.set('.hero-figure', { opacity: 0, translateY: '15%', rotate: '3deg' })
+	gsap.set('.c-hero__text-heading', { opacity: 0, translateY: '15%', rotate: '1deg' })
+	gsap.set('.c-hero__text-subtitle', { opacity: 0, translateY: '15%', rotate: '1deg' })
+	gsap.set('.c-hero__picture', { opacity: 0, translateY: '15%', rotate: '3deg' })
 }
 </script>
 
 <template>
-  <div class="section-hero" @mousemove="({ clientX, clientY }) => (posTrackMouse = { clientX, clientY })">
-    <div class="c-container">
-      <div class="flex flex-col justify-between gap-4">
-        <div class="c-hero">
-          <!-- Hero: Text -->
-          <TheHeroText />
-          <!-- Hero: Figure -->
-          <TheHeroFigure />
-          <!-- Hero: Circles -->
-          <TheHeroCircle :posTrackMouse="posTrackMouse" />
-        </div>
-
-        <TheHeroContact />
-      </div>
+  <div class="section-hero c-container">
+    <div class="c-hero">
+      <TheHeroText />
+      <TheHeroPicture />
+      <TheHeroSpinner />
+      <TheHeroContact />
     </div>
   </div>
 </template>
@@ -69,32 +56,36 @@ const doResetAnimation = (): void => {
 <style lang="scss">
 .section-hero {
   position: relative;
-  padding-top: 4rem;
-  padding-bottom: 4rem;
   width: 100%;
-  min-height: 100svh;
+  height: calc(100svh - 6.5rem);
   background-color: $color-white;
   color: $color-woodsmoke-default;
-  // overflow: hidden;
+
+  @media screen and (min-width: $breakpoint-screen-lg) {
+    padding-bottom: 8rem;
+  }
+
+  @media screen and (min-width: $breakpoint-screen-xl) {
+    padding-bottom: 6rem;
+  }
+
+  @media screen and (min-width: $breakpoint-screen-3xl) {
+    padding-bottom: 12rem;
+  }
 }
 
 .c-hero {
   position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
 
-  // Screen Under Large
-  @media screen and (max-width: $breakpoint-screen-lg) {
-    display: flex;
-    flex-direction: column-reverse;
-    gap: 1.5rem;
-  }
-
-  // Screen Larger
   @media screen and (min-width: $breakpoint-screen-lg) {
     display: grid;
-    grid-template-rows: repeat(8, 1fr);
-    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(12, minmax(0, 1fr));
+    grid-template-columns: repeat(2, 4rem) repeat(10, minmax(0, 1fr));
     gap: 1rem;
-    min-height: 525px;
+    height: 100%;
   }
 }
 </style>
