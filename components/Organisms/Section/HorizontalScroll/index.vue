e3343fee251805a513e481fd75f60ce4ac695402
Alex Vasconcelos <alex.vasconcelos@convenia.com.br>
2025-02-20 23:17:11 -0300

    Refactor Heading and HorizontalScroll components; remove unused code and update GSAP animations

--- components/Organisms/Section/HorizontalScroll/index.vue
+++ components/Organisms/Section/HorizontalScroll/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, ref, useTemplateRef, onMounted, onUpdated } from 'vue'
+import { ref, useTemplateRef, onMounted, onUpdated } from 'vue'
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import { chunk } from 'lodash'
 import gsap from 'gsap'
@@ -13,13 +13,11 @@ import horizontalItems from '@/src/data/horizontalSingle'
 
 const breakpoints = useBreakpoints(breakpointsTailwind)
 const isGreaterOrEqualLarge = breakpoints.greaterOrEqual('lg')
-console.log(isGreaterOrEqualLarge.value)
-// const laptop = breakpoints.between('laptop', 'desktop')
+
 // Data
 const root = useTemplateRef('root')
 const pinned = useTemplateRef('pinned')
 const horizontalRows = useTemplateRef('singleRow')
-const globalRoot = inject('globalRoot')
 const items = ref([])
 // eslint-disable-next-line no-undef
 const img = useImage()
@@ -45,7 +43,7 @@ const createHorizontalRow = (elementss, index, trigger) => {
 	// const elPinned = pinned.value.querySelector('.horizontal-items')
 	// const start = trigger.getBoundingClientRect().top + 128 * (index + 1)
 	// const cols = elements[0].querySelectorAll('.horizontal-single__col')
-	console.log(root.value)
+
 	const tl = gsap
 		.timeline({
 			scrollTrigger: {
@@ -60,7 +58,7 @@ const createHorizontalRow = (elementss, index, trigger) => {
 		})
 		.addLabel('start')
 
-    	// const tlOpacity = gsap
+	// const tlOpacity = gsap
 	// 	.timeline({
 	// 		scrollTrigger: {
 	// 			id: 'opacity',
@@ -85,7 +83,7 @@ const createHorizontalRow = (elementss, index, trigger) => {
 	tl.to(
 		elements,
 		{
-			x: (index) => index % 2 ? '-22vw' : '25vw',
+			x: index => (index % 2 ? '-15vw' : '15vw'),
 			duration: 1
 		},
 		'start'
@@ -146,31 +144,76 @@ onUpdated(() => {
 </template>
 
 <style lang="scss" scoped>
-@media screen and (min-width: $breakpoint-screen-xl) {
+// .horizontal-items {
+//   .horizontal-single__row {
+//     position: relative;
+//     display: grid;
+//     grid-template-columns: repeat(2, 1fr);
+//     grid-auto-rows: 50vh;
+//     gap: 1.5em;
+//   }
+
+//   .horizontal-single__col {
+//     width: 50vw;
+//     height: 50vh;
+
+//     &:first-child {
+//       grid-column: 1 / 2;
+//       grid-row: 1 / 2;
+//     }
+
+//     &:not(:first-child) {
+// display: none;
+//     }
+
+//     .horizontal-single__project {
+//       position: absolute;
+//       inset: 0;
+//       height: 100%;
+//       width: 100%;
+//       background-position: center center;
+//       background-repeat: no-repeat;
+//       background-size: cover;
+//       border-radius: 16px;
+//     }
+//   }
+// }
+
+@media screen and (min-width: $breakpoint-screen-lg) {
   .horizontal-items {
     position: relative;
-    --section-padding: clamp(5em, 21vh, 12em);
+    width: 100%;
+    height: 100vh;
+
+    --horizontal-item-width: 120vw;
+    --items-per-row: 2;
     --gap-padding: clamp(1.5em, 4vw, 2.5em);
 
+    // --horizontal-item-width: 120vw;
+    // --items-per-row: 4;
+    // --gap-padding: clamp(1.5em, 4vw, 2.5em);
+
     .horizontal-single__row {
+      position: relative;
       display: flex;
       flex-wrap: wrap;
-      position: relative;
+      align-items: center;
       width: 120vw;
+      height: 50vh;
       will-change: transform;
 
       &:first-child {
-        left: -25vw;
+        left: -15vw;
       }
 
       &:last-child {
-        left: 0;
+        left: 15vw;
       }
     }
 
     .horizontal-single__col {
-      width: 25%;
-      height: 500px;
+      width: calc(var(--horizontal-item-width) / var(--items-per-row));
+      height: 100%;
       padding: calc(var(--gap-padding) / 2);
       padding: 1.25vw;
       will-change: transform;
@@ -178,17 +221,12 @@ onUpdated(() => {
 
     .horizontal-single__item {
       position: relative;
-
-      &::before {
-        content: '';
-        display: block;
-        padding-top: 75%;
-      }
+      width: 100%;
+      height: 100%;
 
       & > .horizontal-single__project {
         position: absolute;
-        top: 0;
-        left: 0;
+        inset: 0;
         height: 100%;
         width: 100%;
         background-position: center center;
@@ -198,8 +236,7 @@ onUpdated(() => {
 
         & > video {
           position: absolute;
-          top: 0;
-          left: 0;
+          inset: 0;
           height: 100%;
           width: 100%;
           object-fit: cover;
